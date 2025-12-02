7572a36c8f5e0290e0dc2a0675cce755aaff6c6c
JakeB0126 <jwb141@pitt.edu>
2025-12-01 19:56:08 -0500

    Update PlaylistRecommenderTest.java

--- src/test/java/edu/pitt/se/PlaylistRecommenderTest.java
+++ src/test/java/edu/pitt/se/PlaylistRecommenderTest.java
@@ -2,11 +2,29 @@ package edu.pitt.se;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 public class PlaylistRecommenderTest {
+    @Test
+    public void testClassEnergyHigh() {
+        List<Integer> bpms = List.of(170, 130, 150, 150);
+        assertEquals("HIGH", PlaylistRecommender.classifyEnergy(bpms));
+    }
 
     @Test
-    public void placeholder() {
-        assertTrue(true);
+    public void testInvalidTitle() {
+        assertFalse(PlaylistRecommender.isValidTrackTitle("#1 in the World"));
     }
+
+    @Test
+    public void testValidTitle() {
+        assertTrue(PlaylistRecommender.isValidTrackTitle("Key 2 my Heart"));
+    }
+
+    @Test 
+    public void testNormalizeOutofRange(){
+        assertEquals(100, PlaylistRecommender.normalizeVolume(160));
+        assertEquals(0, PlaylistRecommender.normalizeVolume(-5));
+    }
+
 }
